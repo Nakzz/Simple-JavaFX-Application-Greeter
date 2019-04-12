8ea65919e81a401af604d7d54a7567697154476a
nakzz <naqibprio10@gmail.com>
2019-04-11 20:16:59 -0500

    Added more requirements

---
+++ README.txt
@@ -0,0 +1,9 @@
+# Simple-JavaFX-Application-Greeter
+
+Course: cs400
+Semester: Spring 2019
+Project name: JavaFX Greeter
+Team Members:
+1. Ajmain Naqib, 002, naqib@wisc.edu
+
+Notes or comments to the grader:
--- application/Main.java
+++ application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.stage.Stage;
@@ -14,100 +14,99 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
-import javafx.event.ActionEvent; 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+/**
+ * Title: JavaFX Application Course: CS400, Spring 2019 Author: Ajmain Naqib Email: naqib@wisc.edu
+ * Lecturer's Name: Deb Deppeler
+ * 
+ */
+
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-//		  Label greetingLabel = new Label("Hello World!");
-		  Label projectLabel = new Label("CS400 MyFirstJavaFX");
-		  
-		    // Weekdays 
-	        String week_days[] = 
-	                   { "Monday", "Tuesday", "Wednesday", 
-	                                    "Thrusday", "Friday" }; 
-	  
-	        // Create a combo box 
-	        ComboBox combo_box = 
-	                     new ComboBox(FXCollections 
-	                                 .observableArrayList(week_days)); 
-	  
-	  
-	     // load the image
-	         Image image = new Image("aj.jpg");
-	 
-	         // simple displays ImageView the image as is
-	         ImageView iv1 = new ImageView();
-	         iv1.setImage(image);
-	         iv1.setImage(image);
-	         iv1.setFitWidth(100);
-	         iv1.setPreserveRatio(true);
-	         iv1.setSmooth(true);
-	         iv1.setCache(true);
+  @Override
+  public void start(Stage primaryStage) {
+    try {
+      // Label greetingLabel = new Label("Hello World!");
+      Label projectLabel = new Label("CS400 MyFirstJavaFX");
 
-	      // create a button 
-	         Button b = new Button("Done"); 
-	   
-	         //create a text field
-	
-	         TextField textField = new TextField(); 
-	         textField.setText("Enter your Name");
+      // Weekdays
+      String week_days[] = {"Monday", "Tuesday", "Wednesday", "Thrusday", "Friday"};
 
-	         
-	         // create a label 
-	         Text l = new Text(20,50, "Pick a day from checkdown box and enter your name in the text field"); 
-	   
-           l.setWrappingWidth(200);
-           
-//	         l.setWrapText(true);
-	         
-	         // action event 
-	         EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
-	             public void handle(ActionEvent e) 
-	             {     
-	               if(textField.getText().equalsIgnoreCase("Enter your Name")) {
-	                 l.setText("Please enter your name!");
-	               } else if(combo_box.getValue() == null) {
-	                 l.setText("Please pick a day from checkdown box");
-	               }else {
-	                 l.setText( "Hello " +textField.getText() +"! Happy " + combo_box.getValue() + "!"); 
-	             } }
-	         }; 
-	   
-	         // when enter is pressed 
-	         b.setOnAction(event); 
-	         
-	         BorderPane hbox = new BorderPane();
-//	         hbox.getChildren().addAll(textField, l);
-	         
-	         hbox.setTop(textField);
-	         hbox.setBottom(l);
-	         
-	         
-		  primaryStage.setTitle("AJ's Greeter JavaFX program");
-		  
-			BorderPane root = new BorderPane();
-			
-			root.setTop(projectLabel);
-			root.setLeft(combo_box);
-			root.setCenter(iv1);
-			root.setBottom(b);
-			root.setRight(hbox);
-			
-			Scene scene = new Scene(root,400,400);
+      // Create a combo box
+      ComboBox combo_box = new ComboBox(FXCollections.observableArrayList(week_days));
 
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+      // load the image
+      Image image = new Image("aj.jpg");
+
+      // simple displays ImageView the image as is
+      ImageView iv1 = new ImageView();
+      iv1.setImage(image);
+      iv1.setFitWidth(150);
+      iv1.setPreserveRatio(true);
+      iv1.setSmooth(true);
+      iv1.setCache(true);
+
+      // create a button
+      Button b = new Button("Done");
+
+      // create a text field
+      TextField textField = new TextField();
+      textField.setText("Enter your Name");
+      textField.setMaxWidth(150);
+
+      // create a label
+      Text l =
+        new Text(20, 50, "Pick a day from checkdown box and enter your name in the text field");
+
+      l.setWrappingWidth(200);
+
+      // action event
+      EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+        public void handle(ActionEvent e) {
+          if (textField.getText().equalsIgnoreCase("Enter your Name")) {
+            l.setText("Please enter your name!");
+          } else if (combo_box.getValue() == null) {
+            l.setText("Please pick a day from checkdown box");
+          } else {
+            l.setText("Hello " + textField.getText() + "! Happy " + combo_box.getValue() + "!");
+          }
+        }
+      };
+
+      // when enter is pressed
+      b.setOnAction(event);
+
+      BorderPane hbox = new BorderPane();
+      // hbox.getChildren().addAll(textField, l);
+
+      hbox.setTop(textField);
+      hbox.setBottom(l);
+
+
+      primaryStage.setTitle("AJ's Greeter JavaFX program");
+
+      BorderPane root = new BorderPane();
+
+      root.setTop(projectLabel);
+      root.setLeft(combo_box);
+      root.setCenter(iv1);
+      root.setBottom(b);
+      root.setRight(hbox);
+
+      Scene scene = new Scene(root, 400, 400);
+
+      scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
